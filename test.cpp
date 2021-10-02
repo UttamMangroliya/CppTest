cf96e85ac65ecb5a7d02815d3bc9f9d4b1e61151
UttamMangroliya <uttampatel8246@gmail.com>
2021-10-02 10:37:41 +0530

    New File

--- test.cpp
+++ test.cpp
@@ -6,14 +6,11 @@ int main()
 { 
     for(int i=1;i<=5;i++)
     {
-        for(int j=i;j<=5;j++)
+        for(int j=0;j<i;j++)
         {
             cout<<"*";
         }
 
         cout<<"\n";
     }
-
-
-    cout <<"Hello World !!";
 }
