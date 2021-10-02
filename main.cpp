5e34d20abde9bff3ea666ee2f4714f9cbd94b381
UttamMangroliya <uttampatel8246@gmail.com>
2021-10-02 10:34:01 +0530

    Pattern

--- main.cpp
+++ main.cpp
@@ -3,6 +3,14 @@
 using namespace std;
 
 int main()
-{
-    cout<<"Hello World!!";
+{ 
+    for(int i=1;i<=5;i++)
+    {
+        for(int j=i;j<=5;j++)
+        {
+            cout<<"*";
+        }
+
+        cout<<"\n";
+    }
 }
